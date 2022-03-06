3a54cf80e8e24143301c40bb9fc6e9c10c1838b7
Sam Brentnall <sam.brentnall@gmail.com>
2022-03-06 17:06:08 +0000

    renamed workflow and removed tf state location in main file

--- azure-resource-group/main.tf
+++ azure-resource-group/main.tf
@@ -12,16 +12,6 @@ provider "azurerm" {
   features {}
 }
 
-# save tf state in container
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state"
-    storage_account_name = "sambtfstorageaccount"
-    container_name       = "terraformstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
 # Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resourcename}ResourceGroup"
