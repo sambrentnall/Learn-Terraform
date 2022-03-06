54905bdd9e8e4738a41d35b67331bfbb165acdcd
Sam Brentnall <sam.brentnall@gmail.com>
2022-03-06 13:29:14 +0000

    added workflow items for pipeline testing

--- azure-resource-group/main.tf
+++ azure-resource-group/main.tf
@@ -1,3 +1,13 @@
+# Use Azure Storage Container for .tfstate file
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "sambfstates"
+    storage_account_name = "sambtf"
+    container_name       = "sambdevops"
+    key                  = "sambdevops.tfstate"
+  }
+}
+
 # Configure the Azure provider
 terraform {
   required_providers {
@@ -14,6 +24,9 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
+# Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "Terraform_RG"
   location = "uksouth"
